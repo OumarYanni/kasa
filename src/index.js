8e897880d2cd2d8e42bcefcf0ae5db8e8093653f
OumarYanni <oumaryanni@gmail.com>
2023-02-20 11:13:00 +0000

    Intégration page 404

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
 import Apropos from "./pages/Apropos";
+import Untraceable from "./pages/Untraceable";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -13,6 +14,7 @@ root.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Apropos" element={<Apropos />} />
+        <Route path="*" element={<Untraceable />} />
       </Routes>
     </Router>
   </React.StrictMode>
